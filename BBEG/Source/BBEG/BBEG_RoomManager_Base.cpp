648ebcf57678a6ed5f6d561f3caca6aaa451946c
henry.chronowski <hrchronowski@gmail.com>
2023-10-06 14:31:53 -0400

    Adding cpp room manager base

---
+++ BBEG/Source/BBEG/BBEG_RoomManager_Base.cpp
@@ -0,0 +1,27 @@
+// Copyright Earthbreak Games LLC 2023
+
+
+#include "BBEG_RoomManager_Base.h"
+
+// Sets default values
+ABBEG_RoomManager_Base::ABBEG_RoomManager_Base()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABBEG_RoomManager_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABBEG_RoomManager_Base::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
